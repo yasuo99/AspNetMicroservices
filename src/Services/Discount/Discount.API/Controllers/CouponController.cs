075de8224fcbf61bb0bdc6959b878795a0c55b50
yasuo99 <yasuo12091999@gmail.com>
2022-01-11 15:37:01 +0700

    Add Discount service with Postgre

---
+++ src/Services/Discount/Discount.API/Controllers/CouponController.cs
@@ -0,0 +1,94 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CouponController : ControllerBase
+    {
+        private readonly ICouponRepository _repo;
+        public CouponController(ICouponRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("[action]/{productName}", Name = "GetCoupons")]
+        [ProducesResponseType(typeof(IEnumerable<Coupon>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCouponsByProductName(string productName)
+        {
+            return Ok(await _repo.GetCoupons(productName));
+        }
+
+        [HttpGet("{id}", Name = "GetCoupon")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCoupon(int id)
+        {
+            var coupon = await _repo.GetCoupon(id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> CreateCoupon([FromBody] Coupon coupon)
+        {
+            var result = await _repo.CreateCoupon(coupon);
+
+            if (!result)
+            {
+                return NoContent();
+            }
+            return CreatedAtAction("GetCoupon", new { id = coupon.Id }, coupon);
+        }
+        [HttpPut("{id}", Name = "UpdateCoupon")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateCoupon(int id, [FromBody] Coupon coupon)
+        {
+            if (id != coupon.Id)
+            {
+                return BadRequest();
+            }
+            var result = await _repo.UpdateCoupon(coupon);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok(coupon);
+        }
+        [HttpDelete("{id}", Name = "DeleteCoupon")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteCoupon(int id)
+        {
+            var coupon = await _repo.GetCoupon(id);
+            if(coupon == null)
+            {
+                return NotFound();
+            }
+            return Ok(await _repo.DeleteCoupon(id));
+        }
+        [HttpDelete("[action]/{productName}", Name = "DeleteCouponByProductName")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteCouponByProductName(string productName)
+        {
+            return Ok(await _repo.DeleteCouponByProductName(productName));
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/ICouponRepository.cs
@@ -0,0 +1,19 @@
+﻿using Discount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface ICouponRepository
+    {
+        Task<IEnumerable<Coupon>> GetCoupons(string productName);
+        Task<Coupon> GetCoupon(int id);
+        Task<bool> CreateCoupon(Coupon coupon);
+        Task<bool> UpdateCoupon(Coupon coupon);
+        Task<bool> DeleteCouponByProductName(string productName);
+        Task<bool> DeleteCoupon(int id);
+    }
+}
